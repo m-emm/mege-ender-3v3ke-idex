c4a8b7979e9a46cc471e9ae1402d8da5aee7b18c
Markus Emmenegger <markus@emmenegger.net>
2026-01-16 15:02:49 +0100

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+    Dummy conftest.py for mege_ender_3v3ke_idex.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    - https://docs.pytest.org/en/stable/fixture.html
+    - https://docs.pytest.org/en/stable/writing_plugins.html
+"""
+
+# import pytest
