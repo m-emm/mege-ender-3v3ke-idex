2737dc6d758b0539ee29c451f8e52b1a8b351c87
Markus Emmenegger <markus@emmenegger.net>
2026-02-15 13:18:42 +0100

    Reconstruct cage

--- src/mege_ender_3v3ke_idex/designs/jury_rigged_z_carriage.py
+++ src/mege_ender_3v3ke_idex/designs/jury_rigged_z_carriage.py
@@ -52,8 +52,9 @@ wheel_distance = 36
 wheel_front_y_offset = 6.5
 wheels_vertical_gap = 8
 wheels_plate_thickness = 8
-wheels_plate_border = 5
+wheels_plate_border = 8
 wheels_plate_gap = 2
+wheels_plate_preload_gap = 1.6
 wheel_axle_diameter = 8
 wheel_axle_length = 100
 wheel_axle_clearance = 0.2
@@ -76,15 +77,21 @@ plate_connector_screw_size = "M3"
 screw_connector_length = 25
 
 cage_wall = 3
-cage_height = 20
-cage_flange_thickness = 4
-cage_flange_width = 7
+cage_height = 30
+
 cage_front_gap = 2
 
 cage_screw_size = "M3"
 
 cage_screw_inset = 5
 cage_screw_drill_depth = 5
+
+cage_enhancer_width = 7
+cage_enhancer_thickness = 7
+hidden_nut_pocket_slack = 0.35
+
+
+shortened_step_export_path = "/Users/mege/git/mege-ender-3v3ke-idex/resources/step_experiments/creality_z_axis_one_side_only_shortened.step"
 
 
 def create_creality_z_axis():
@@ -94,6 +101,43 @@ def create_creality_z_axis():
     imported = import_solid_from_step(step_path)
     imported = rotate(90, axis=(1, 0, 0))(imported)  # Rotate to correct orientation
     _logger.info("Z axis imported successfully.")
+
+    return imported
+
+
+def create_creality_z_axis_one_side_only_shortened():
+    step_path = "/Users/mege/git/mege-ender-3v3ke-idex/resources/step_experiments/zaxis_only.step"
+
+    _logger.info(f"Importing Z axis from STEP file: {step_path}")
+    imported = import_solid_from_step(step_path)
+
+    _logger.info("Z axis imported successfully.")
+
+    imported = rotate(90, axis=(1, 0, 0))(imported)  # Rotate to correct orientation
+    retval, _ = cut_in_two(imported, cut_normal=(1, 0, 0))
+
+    retval_bbox = get_bounding_box(retval)
+
+    cut_point_z = retval_bbox[0][2] + 120
+    _, retval = cut_in_two(retval, cut_normal=(0, 0, 1), cut_point=(0, 0, cut_point_z))
+
+    export_solid_to_step(retval, shortened_step_export_path)
+    _logger.info(
+        f"Exported shortened Z axis to STEP for later re-use: {shortened_step_export_path}"
+    )
+
+    _logger.info("Z axis prepared successfully.")
+
+    return retval
+
+
+def create_creality_z_axis_one_side_only_shortened_from_step():
+
+    _logger.info(
+        f"Importing shortened Z axis from STEP file: {shortened_step_export_path}"
+    )
+    imported = import_solid_from_step(shortened_step_export_path)
+    _logger.info("Shortened Z axis imported successfully.")
 
     return imported
 
@@ -143,7 +187,9 @@ def create_screww_connector(screw_size, length):
     connector = create_box(length, connector_side, connector_side)
 
     nut_socked_cutter = create_hidden_nut_pocket_cutter(
-        screw_size, bottom_cutter_length=connector_side / 2
+        screw_size,
+        bottom_cutter_length=connector_side / 2,
+        slack=hidden_nut_pocket_slack,
     )
     nut_socked_cutter = rotate(90)(nut_socked_cutter)
     nut_socked_cutter = rotate(90, axis=(0, 1, 0))(nut_socked_cutter)
@@ -159,7 +205,7 @@ def create_screww_connector(screw_size, length):
     return connector
 
 
-def create_jury_rigged_z_carriage(z_axis_profile):
+def create_jury_rigged_z_carriage(z_axis_profile, threaded_rod):
     """Create the jury_rigged_z_carriage part."""
 
     wheels = LeaderFollowersCuttersPart(PartCollector())
@@ -317,7 +363,7 @@ def create_jury_rigged_z_carriage(z_axis_profile):
 
     plate_size = get_bounding_box_size(wheels_plate)
 
-    cage_front_wall = create_box(plate_size[0], cage_wall, cage_height)
+    cage_front_wall = create_box(plate_size[0] + 2 * cage_wall, cage_wall, cage_height)
     cage_front_wall = align(cage_front_wall, wheels_plate, Alignment.CENTER)
     cage_front_wall = align(
         cage_front_wall, z_axis_profile, Alignment.STACK_FRONT, stack_gap=cage_front_gap
@@ -327,45 +373,52 @@ def create_jury_rigged_z_carriage(z_axis_profile):
     for lr in [Alignment.RIGHT, Alignment.LEFT]:
         cage_side_wall = create_box(cage_wall, BIG_THING, cage_height)
         cage_side_wall = align(cage_side_wall, cage_front_wall, Alignment.CENTER)
-        cage_side_wall = align(cage_side_wall, cage_front_wall, lr.stack_alignment)
-        cage_side_wall = align(cage_side_wall, cage_front_wall, Alignment.FRONT)
+        cage_side_wall = align(cage_side_wall, cage_front_wall, lr)
+        cage_side_wall = align(cage_side_wall, cage_front_wall, Alignment.STACK_BACK)
 
         cage_side_wall = cage_side_wall.cut(wheels_plate_back_cutter)
 
+        side_wall_enhancers = PartCollector()
         for tb in [Alignment.TOP, Alignment.BOTTOM]:
-            drll_radius = MScrew.from_size(cage_screw_size).core_hole / 2
-            screw_drill = create_cylinder(drll_radius, BIG_THING)
 
-            screw_drill = rotate(90, axis=(0, 1, 0))(screw_drill)
-
-            screw_drill = align(screw_drill, cage_side_wall, Alignment.CENTER)
-            screw_drill = align(
-                screw_drill, wheels_plate_plain, Alignment.CENTER, axes=[1]
+            enhancer = create_box(
+                cage_enhancer_width, BIG_THING, cage_enhancer_thickness
             )
+            enhancer = align(enhancer, cage_side_wall, Alignment.CENTER)
+            enhancer = align(enhancer, cage_side_wall, tb)
+            enhancer = align(enhancer, cage_side_wall, lr.opposite.stack_alignment)
+            enhancer = align(enhancer, cage_side_wall, Alignment.FRONT)
+            enhancer = enhancer.cut(wheels_plate_back_cutter)
 
-            screw_drill = align(
-                screw_drill,
-                cage_side_wall,
-                tb.stack_alignment,
-                stack_gap=-cage_screw_inset - drll_radius,
+            nut_socked_cutter = create_hidden_nut_pocket_cutter(
+                cage_screw_size, bottom_cutter_length=4, slack=hidden_nut_pocket_slack
             )
+            nut_socked_cutter = rotate(-lr.sign * 90)(nut_socked_cutter)
+            nut_socked_cutter = rotate(90, axis=(1, 0, 0))(nut_socked_cutter)
 
-            screw_drill = align(
-                screw_drill,
-                cage_side_wall,
-                lr.stack_alignment,
-                stack_gap=-cage_screw_drill_depth - cage_wall,
-            )
+            nut_socked_cutter = align(nut_socked_cutter, enhancer, Alignment.CENTER)
+            nut_socked_cutter = align(nut_socked_cutter, enhancer, Alignment.FRONT)
+            nut_size = get_bounding_box_size(nut_socked_cutter)
 
-            cage_side_wall = cage_side_wall.cut(screw_drill)
+            nut_socked_cutter = translate(0, 1.5 * nut_size[1], 0)(nut_socked_cutter)
+            enhancer = nut_socked_cutter.use_as_cutter_on(enhancer)
 
-            retval = retval.cut(screw_drill)
+            cage_front_wall = nut_socked_cutter.use_as_cutter_on(cage_front_wall)
+            cage_side_wall = nut_socked_cutter.use_as_cutter_on(cage_side_wall)
+
+            side_wall_enhancers = side_wall_enhancers.fuse(enhancer)
+
+        cage_side_wall = cage_side_wall.fuse(side_wall_enhancers)
 
         cage_side_walls = cage_side_walls.fuse(cage_side_wall)
 
-    cage = cage_front_wall.fuse(cage_side_walls)
+    cage = cage_front_wall
 
-    left_plate, right_plate = cut_in_two(retval, cut_normal=(1, 0, 0))
+    retval = retval.fuse(cage_side_walls)
+
+    left_plate, right_plate = cut_in_two(
+        retval, cut_normal=(1, 0, 0), cut_thickness=wheels_plate_preload_gap
+    )
 
     retval.add_named_follower(left_plate, "left_part")
     retval.add_named_follower(right_plate, "right_part")
@@ -379,10 +432,37 @@ def main():
     logging.basicConfig(level=logging.INFO)
     parts = PartList()
 
-    z_axis = create_creality_profile_segment_only_from_step()
-    parts.add(z_axis, "creality_z_axis", flip=False, skip_in_production=True)
+    z_axis_profile = create_creality_profile_segment_only_from_step()
 
-    z_carriage = create_jury_rigged_z_carriage(z_axis)
+    short_z_axis_one_side = create_creality_z_axis_one_side_only_shortened_from_step()
+
+
+    short_z_axis_one_side = align(
+        short_z_axis_one_side, z_axis_profile, Alignment.CENTER
+    )
+    short_z_axis_one_side = align(
+        short_z_axis_one_side, z_axis_profile, Alignment.FRONT
+    )
+    short_z_axis_one_side = align(
+        short_z_axis_one_side, z_axis_profile, Alignment.BOTTOM
+    )
+    parts.add(
+        short_z_axis_one_side, "creality_z_axis", flip=False, skip_in_production=True
+    )
+
+    short_z_axis_bb = get_bounding_box(short_z_axis_one_side)
+
+    threaded_rod, _ = cut_in_two(
+        short_z_axis_one_side,
+        cut_normal=(0, 1, 0),
+        cut_point=(0, short_z_axis_bb[1][1] - 15, 0),
+    )
+
+    #threaded_rod = translate(0, 0, 150)(threaded_rod)
+    #parts.add(threaded_rod, "threaded_rod", flip=False, skip_in_production=True)
+
+
+    z_carriage = create_jury_rigged_z_carriage(z_axis_profile, threaded_rod)
 
     for name in ["left_part", "right_part"]:
         follower = z_carriage.get_follower_part_by_name(name)
@@ -415,6 +495,7 @@ def main():
         prod=PROD,
         process_data=PROCESS_DATA,
         prod_gap=5,
+        export_individual_parts=False,
     )
 
     _logger.info("jury_rigged_z_carriage created successfully!")
