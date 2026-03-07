55dbecd202ee682e687823aebe1bded9700eecf9
Markus Emmenegger <markus@emmenegger.net>
2026-03-07 22:55:48 +0100

    z axis carriage

--- src/mege_ender_3v3ke_idex/designs/z_axis.py
+++ src/mege_ender_3v3ke_idex/designs/z_axis.py
@@ -58,6 +58,44 @@ PROCESS_DATA["process_overrides"].update(
 z_axis_threaded_rod_diameter = 8
 z_axis_threaded_rod_length = 500
 
+z_axis_guide_rod_length = 600
+z_axis_guide_rod_diameter = 8
+
+z_axis_profile_length = 700
+
+z_axis_guide_rod_threaded_rod_distance = 18
+z_axis_guide_rod_profile_distance = 50
+
+z_axis_carriage_height = 60
+z_axis_carriage_width = 45
+z_axis_carriage_depth = 50
+z_axis_carriage_fillet_radius = 4
+z_axis_carriage_mount_screw_size = "M3"
+z_axis_carriage_bearing_inset = 5
+z_axis_carriage_threaded_rod_clearance = 0.3
+z_axis_carriage_profile_clearance = 2
+
+igus_drylin_bearing_inner_diameter = 8
+igus_drylin_bearing_outer_diameter = 16
+igus_drylin_bearing_length = 25
+
+
+def create_igus_drylin_bearing(cutter_clearance=0.1, cutter_extra_length=2):
+    bearing = create_ring(
+        igus_drylin_bearing_outer_diameter / 2,
+        igus_drylin_bearing_inner_diameter / 2,
+        igus_drylin_bearing_length,
+    )
+    cutter = create_cylinder(
+        (igus_drylin_bearing_outer_diameter / 2) + cutter_clearance,
+        igus_drylin_bearing_length + cutter_extra_length,
+    )
+    cutter = align(cutter, bearing, Alignment.CENTER)
+
+    retval = LeaderFollowersCuttersPart(bearing, cutters=[cutter])
+
+    return retval
+
 
 def create_creality_threaded_rod_nut():
     base_thickness = 3.5
@@ -131,26 +169,84 @@ def create_creality_threaded_rod_nut():
     return retval
 
 
+def create_carriage(guide_rod, threaded_rod, profile):
+    carriage = create_filleted_box(
+        z_axis_carriage_width,
+        z_axis_carriage_depth,
+        z_axis_carriage_height,
+        z_axis_carriage_fillet_radius,
+    )
+
+    carriage = align(carriage, guide_rod, Alignment.CENTER)
+    carriage = align(carriage, guide_rod, Alignment.BOTTOM)
+    carriage = align(
+        carriage,
+        profile,
+        Alignment.STACK_FRONT,
+        stack_gap=z_axis_carriage_profile_clearance,
+    )
+
+    bearing = create_igus_drylin_bearing(
+        cutter_clearance=0.1, cutter_extra_length=z_axis_carriage_height
+    )
+    bearing = align(bearing, carriage, Alignment.CENTER)
+    bearing = align(bearing, guide_rod, Alignment.CENTER, axes=[0, 1])
+
+    carriage = bearing.use_as_cutter_on(carriage)
+
+    top_bearing = align(bearing, carriage, Alignment.TOP)
+
+    threaded_rod_cutter = create_cylinder(
+        z_axis_threaded_rod_diameter / 2 + z_axis_carriage_threaded_rod_clearance,
+        z_axis_carriage_height + 10,
+    )
+
+    threaded_rod_cutter = align(threaded_rod_cutter, carriage, Alignment.CENTER)
+    threaded_rod_cutter = align(
+        threaded_rod_cutter, threaded_rod, Alignment.CENTER, axes=[0, 1]
+    )
+
+    carriage = carriage.cut(threaded_rod_cutter)
+
+    retval = LeaderFollowersCuttersPart(carriage)
+
+    retval.add_named_non_production_part(top_bearing, "top_bearing")
+
+    bottom_bearing = align(bearing, carriage, Alignment.BOTTOM)
+    retval.add_named_non_production_part(bottom_bearing, "bottom_bearing")
+
+    return retval
+
+
 def create_z_axis():
     """Create the z_axis part."""
-    # Example: simple box with a cylindrical hole
-    width = 30
-    depth = 20
-    height = 10
-    hole_radius = 4
 
-    # Create base box
-    part = create_box(width, depth, height)
+    z_axis_profile = create_alu_extrusion_profile(
+        ExtrusionProfileType.PROFILE_4040, length_mm=z_axis_profile_length
+    )
 
-    # Create a hole cutter
-    hole = create_cylinder(hole_radius, height + 2)
-    hole = align(hole, part, Alignment.CENTER)
-    hole = translate(0, 0, -1)(hole)
+    guide_rod = create_cylinder(z_axis_guide_rod_diameter / 2, z_axis_guide_rod_length)
 
-    # Cut the hole
-    part = part.cut(hole)
+    guide_rod = align(guide_rod, z_axis_profile, Alignment.CENTER)
+    guide_rod = align(guide_rod, z_axis_profile, Alignment.BOTTOM)
 
-    return part
+    guide_rod = translate(0, -z_axis_guide_rod_profile_distance, 0)(guide_rod)
+
+    threaded_rod = create_cylinder(
+        z_axis_threaded_rod_diameter / 2, z_axis_threaded_rod_length
+    )
+
+    threaded_rod = align(threaded_rod, guide_rod, Alignment.CENTER)
+    threaded_rod = align(threaded_rod, guide_rod, Alignment.BOTTOM)
+
+    threaded_rod = translate(0, z_axis_guide_rod_threaded_rod_distance, 0)(threaded_rod)
+
+    retval = LeaderFollowersCuttersPart(z_axis_profile)
+
+    retval.add_named_non_production_part(guide_rod, "guide_rod")
+    retval.add_named_non_production_part(threaded_rod, "threaded_rod")
+
+    return retval
 
 
 def main():
@@ -160,8 +256,25 @@ def main():
     logging.basicConfig(level=logging.INFO)
     parts = PartList()
 
-    nut = create_creality_threaded_rod_nut()
-    parts.add(nut, "threaded_rod_nut", flip=False)
+    z_axis = create_z_axis()
+
+    parts.add(z_axis, "z_axis", flip=False)
+
+    for name, npp in z_axis.get_named_non_production_part_items():
+        parts.add(npp, name, flip=False, skip_in_production=True)
+
+    carriage = create_carriage(
+        z_axis.get_named_non_production_part("guide_rod"),
+        z_axis.get_named_non_production_part("threaded_rod"),
+        z_axis,
+    )
+
+    carriage = translate(0, 0, 100)(carriage)
+
+    parts.add(carriage, "z_axis_carriage", flip=False)
+
+    for name, npp in carriage.get_named_non_production_part_items():
+        parts.add(npp, name, flip=False, skip_in_production=True)
 
     # printer_frame = create_printer_frame()
 
