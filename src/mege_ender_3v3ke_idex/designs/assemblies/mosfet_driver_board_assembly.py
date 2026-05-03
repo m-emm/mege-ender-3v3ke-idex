4455dc5a902e02bf2598880221ff7ea2590a7ce9
Markus Emmenegger <markus@emmenegger.net>
2026-05-03 12:40:08 +0200

    Optimize FET driver board

--- src/mege_ender_3v3ke_idex/designs/assemblies/mosfet_driver_board_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/mosfet_driver_board_assembly.py
@@ -1,5 +1,6 @@
 """MOSFET driver board assembly."""
 
+from mege_ender_3v3ke_idex.designs.sil_dil import create_sil
 from shellforgepy.simple import *
 
 
@@ -107,6 +108,15 @@ def create_mosfet_driver_board_assembly(
     mosfet_driver_package_width,
     mosfet_driver_package_height,
     mosfet_driver_package_gap_to_terminal_block,
+    mosfet_driver_j1_row_pin_count,
+    mosfet_driver_j1_hole_diameter,
+    mosfet_driver_j1_four_pin_row_x,
+    mosfet_driver_j1_two_pin_row_x,
+    mosfet_driver_j1_two_pin_y_offset,
+    x_axis_mcu_wire_wrap_pin_side,
+    x_axis_mcu_wire_wrap_pin_length,
+    x_axis_mcu_wire_wrap_pin_base_thickness,
+    x_axis_mcu_top_pin_length,
 ):
     hole_center_offset = (
         mosfet_driver_mount_hole_edge_clearance + mosfet_driver_mount_hole_diameter / 2
@@ -138,6 +148,8 @@ def create_mosfet_driver_board_assembly(
         raise ValueError(
             "MOSFET package width must not exceed the terminal block width."
         )
+    if mosfet_driver_j1_row_pin_count < 2:
+        raise ValueError("mosfet_driver_j1_row_pin_count must be at least 2.")
 
     board = create_box(
         mosfet_driver_board_length,
@@ -159,6 +171,59 @@ def create_mosfet_driver_board_assembly(
         mount_hole = translate(hole_center_offset, mount_hole_y, -1)(mount_hole)
         board = board.cut(mount_hole)
         mount_holes.append(mount_hole)
+
+    j1_row_pitch = 2.54
+    j1_four_pin_y_offsets = [
+        (pin_index - (mosfet_driver_j1_row_pin_count - 1) / 2) * j1_row_pitch
+        for pin_index in range(mosfet_driver_j1_row_pin_count)
+    ]
+    j1_holes = []
+    for pin_index, y_offset in enumerate(j1_four_pin_y_offsets):
+        j1_hole = create_cylinder(
+            mosfet_driver_j1_hole_diameter / 2,
+            mosfet_driver_board_thickness + 2,
+        )
+        j1_hole = translate(
+            mosfet_driver_j1_four_pin_row_x,
+            mosfet_driver_board_width / 2 + y_offset,
+            -1,
+        )(j1_hole)
+        board = board.cut(j1_hole)
+        j1_holes.append((f"j1_row_hole_{pin_index + 1}", j1_hole))
+
+    for hole_name, y_sign in [
+        ("j1_aux_hole_left", -1),
+        ("j1_aux_hole_right", 1),
+    ]:
+        j1_hole = create_cylinder(
+            mosfet_driver_j1_hole_diameter / 2,
+            mosfet_driver_board_thickness + 2,
+        )
+        j1_hole = translate(
+            mosfet_driver_j1_two_pin_row_x,
+            mosfet_driver_board_width / 2
+            + y_sign * mosfet_driver_j1_two_pin_y_offset,
+            -1,
+        )(j1_hole)
+        board = board.cut(j1_hole)
+        j1_holes.append((hole_name, j1_hole))
+
+    j1_connector = create_sil(
+        num_y_pins=mosfet_driver_j1_row_pin_count,
+        pin_length=x_axis_mcu_wire_wrap_pin_length,
+        pin_side=x_axis_mcu_wire_wrap_pin_side,
+        top_pin_length=x_axis_mcu_top_pin_length,
+        base_thickness=x_axis_mcu_wire_wrap_pin_base_thickness,
+        pin_cutter_slack=0.0,
+        base_cutter_slack=0.0,
+        base_cutter_vertical_slack=0.0,
+    )
+    j1_pin_row_center = get_bounding_box_center(j1_connector.cutters[1])
+    j1_connector = translate(
+        mosfet_driver_j1_four_pin_row_x - j1_pin_row_center[0],
+        mosfet_driver_board_width / 2 - j1_pin_row_center[1],
+        0,
+    )(j1_connector)
 
     terminal_block_front = _create_terminal_block(
         terminal_block_length=mosfet_driver_terminal_block_length,
@@ -232,11 +297,18 @@ def create_mosfet_driver_board_assembly(
 
     assembly = LeaderFollowersCuttersPart(leader=board)
     assembly.add_named_follower(board, "board")
+    assembly.add_named_follower(j1_connector.leader, "j1_connector")
+    assembly.add_named_follower(
+        j1_connector.get_follower_part_by_name("top_pins"),
+        "j1_connector_top_pins",
+    )
     assembly.add_named_follower(terminal_block_front, "terminal_block_front")
     assembly.add_named_follower(terminal_block_back, "terminal_block_back")
     assembly.add_named_follower(mosfet_package_front, "mosfet_package_front")
     assembly.add_named_follower(mosfet_package_back, "mosfet_package_back")
     assembly.add_named_cutter(mount_holes[0], "mounting_hole_front")
     assembly.add_named_cutter(mount_holes[1], "mounting_hole_back")
+    for hole_name, j1_hole in j1_holes:
+        assembly.add_named_cutter(j1_hole, hole_name)
 
     return assembly
