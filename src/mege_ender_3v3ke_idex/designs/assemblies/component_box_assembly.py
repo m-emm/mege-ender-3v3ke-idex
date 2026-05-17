46aa5bd780a22e6cd468d5ee8535e96efdde41f0
Markus Emmenegger <markus@emmenegger.net>
2026-05-17 10:20:03 +0200

    Refactor component box to be shorter

--- src/mege_ender_3v3ke_idex/designs/assemblies/component_box_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/component_box_assembly.py
@@ -38,88 +38,6 @@ def _fillet_box(part, fillet_radius, smallest_dimension):
     )
 
 
-def _create_hollow_box(
-    *,
-    outer_length,
-    outer_width,
-    outer_height,
-    inner_length,
-    inner_width,
-    inner_height,
-    bottom_thickness,
-    origin=(0, 0, 0),
-    fillet_radius=0,
-):
-    box = create_box(outer_length, outer_width, outer_height, origin=origin)
-    box = _fillet_box(
-        box,
-        fillet_radius,
-        min(outer_length, outer_width, outer_height, bottom_thickness),
-    )
-
-    inner_origin = (
-        origin[0] + (outer_length - inner_length) / 2,
-        origin[1] + (outer_width - inner_width) / 2,
-        origin[2] + bottom_thickness,
-    )
-    cutter = create_box(
-        inner_length,
-        inner_width,
-        inner_height + bottom_thickness,
-        origin=inner_origin,
-    )
-    return box.cut(cutter)
-
-
-def _create_resistor_visual(*, center):
-    body_start = (center[0] - RESISTOR_BODY_LENGTH / 2, center[1], center[2])
-    wire_start = (center[0] - RESISTOR_WIRE_LENGTH / 2, center[1], center[2])
-
-    body = directed_cylinder_at(
-        body_start,
-        direction=(1, 0, 0),
-        radius=RESISTOR_BODY_DIAMETER / 2,
-        height=RESISTOR_BODY_LENGTH,
-    )
-    wire = directed_cylinder_at(
-        wire_start,
-        direction=(1, 0, 0),
-        radius=RESISTOR_WIRE_DIAMETER / 2,
-        height=RESISTOR_WIRE_LENGTH,
-    )
-    return body.fuse(wire)
-
-
-def _cut_lid_hinge_clearance(
-    lid,
-    *,
-    lid_origin,
-    lid_outer_height,
-    wall_thickness,
-    hinge_x,
-    hinge_width,
-    hinge_depth,
-    hinge_thickness,
-    lid_clearance,
-):
-    cutout_width = hinge_width + 2 * lid_clearance
-    cutout_depth = wall_thickness + hinge_depth + 2 * lid_clearance
-    cutout_height = hinge_thickness + lid_clearance
-    cutout_z = lid_outer_height - cutout_height
-
-    cutout = create_box(
-        cutout_width,
-        cutout_depth,
-        cutout_height + 1e-3,
-        origin=(
-            hinge_x - lid_clearance,
-            lid_origin[1] - lid_clearance,
-            lid_origin[2] + cutout_z,
-        ),
-    )
-    return lid.cut(cutout)
-
-
 def create_component_box_assembly(
     *,
     inner_length,
@@ -176,61 +94,70 @@ def create_component_box_assembly(
             "inner_width and inner_height must fit the resistor body visual"
         )
 
-    body = _create_hollow_box(
-        outer_length=outer_length,
-        outer_width=outer_width,
-        outer_height=body_height,
-        inner_length=inner_length,
-        inner_width=inner_width,
-        inner_height=inner_height,
-        bottom_thickness=floor_thickness,
-        fillet_radius=fillet_radius,
+    body = create_box(outer_length, outer_width, body_height)
+    body = _fillet_box(
+        body,
+        fillet_radius,
+        min(outer_length, outer_width, body_height, floor_thickness),
     )
 
-    lid_y = outer_width + hinge_gap
-    lid_origin = ((outer_length - lid_outer_length) / 2, lid_y, 0)
-    lid = _create_hollow_box(
-        outer_length=lid_outer_length,
-        outer_width=lid_outer_width,
-        outer_height=lid_outer_height,
-        inner_length=lid_inner_length,
-        inner_width=lid_inner_width,
-        inner_height=lid_inner_height,
-        bottom_thickness=lid_thickness,
-        origin=lid_origin,
-        fillet_radius=fillet_radius,
+    body_inner_space_cutter = create_box(inner_length, inner_width, inner_height + 1)
+    body_inner_space_cutter = align(
+        body_inner_space_cutter, body, Alignment.CENTER, axes=[0, 1]
     )
+    body_inner_space_cutter = align(body_inner_space_cutter, body, Alignment.BOTTOM)
+    body_inner_space_cutter = translate(0, 0, floor_thickness)(body_inner_space_cutter)
+    body = body.cut(body_inner_space_cutter)
 
-    hinge_x = (outer_length - hinge_width) / 2
-    lid = _cut_lid_hinge_clearance(
+
+    lid = create_box(lid_outer_length, lid_outer_width, lid_outer_height)
+    lid = _fillet_box(
         lid,
-        lid_origin=lid_origin,
-        lid_outer_height=lid_outer_height,
-        wall_thickness=wall_thickness,
-        hinge_x=hinge_x,
-        hinge_width=hinge_width,
-        hinge_depth=hinge_depth,
-        hinge_thickness=hinge_thickness,
-        lid_clearance=lid_clearance,
+        fillet_radius,
+        min(lid_outer_length, lid_outer_width, lid_outer_height, lid_thickness),
     )
+    lid = align(lid, body, Alignment.CENTER, axes=[0])
+    lid = align(lid, body, Alignment.BOTTOM)
+    lid = align(lid, body, Alignment.STACK_BACK, stack_gap=hinge_gap)
 
-    hinge_y = outer_width - hinge_depth / 2
-    hinge = create_box(
-        hinge_width,
-        hinge_gap + hinge_depth,
-        hinge_thickness,
-        origin=(hinge_x, hinge_y, 0),
+    lid_inner_space_cutter = create_box(
+        lid_inner_length, lid_inner_width, lid_inner_height + 1
     )
+    lid_inner_space_cutter = align(
+        lid_inner_space_cutter, lid, Alignment.CENTER, axes=[0, 1]
+    )
+    lid_inner_space_cutter = align(lid_inner_space_cutter, lid, Alignment.BOTTOM)
+    lid_inner_space_cutter = translate(0, 0, lid_thickness)(lid_inner_space_cutter)
+    lid = lid.cut(lid_inner_space_cutter)
+
+    hinge = create_box(hinge_width, hinge_gap + hinge_depth, hinge_thickness)
+    hinge = align(hinge, body, Alignment.CENTER, axes=[0])
+    hinge = align(hinge, body, Alignment.BOTTOM)
+    hinge = align(hinge, body, Alignment.STACK_BACK, stack_gap=-hinge_depth / 2)
+
+    lid_hinge_clearance = create_box(
+        hinge_width + 2 * lid_clearance,
+        wall_thickness + hinge_depth + 2 * lid_clearance,
+        hinge_thickness + lid_clearance + 1e-3,
+    )
+    lid_hinge_clearance = align(lid_hinge_clearance, hinge, Alignment.CENTER, axes=[0])
+    lid_hinge_clearance = align(lid_hinge_clearance, lid, Alignment.FRONT)
+    lid_hinge_clearance = align(lid_hinge_clearance, lid, Alignment.TOP)
+    lid_hinge_clearance = translate(0, -lid_clearance, 0)(lid_hinge_clearance)
+    lid = lid.cut(lid_hinge_clearance)
+
+    resistor_body = create_cylinder(RESISTOR_BODY_DIAMETER / 2, RESISTOR_BODY_LENGTH)
+    resistor_body = rotate(90, axis=(0, 1, 0))(resistor_body)
+    resistor_body = align(resistor_body, body, Alignment.CENTER)
+    resistor_body = align(resistor_body, body, Alignment.TOP)
+
+    resistor_wire = create_cylinder(RESISTOR_WIRE_DIAMETER / 2, RESISTOR_WIRE_LENGTH)
+    resistor_wire = rotate(90, axis=(0, 1, 0))(resistor_wire)
+    resistor_wire = align(resistor_wire, resistor_body, Alignment.CENTER)
+
+    resistor_visual = resistor_wire.fuse(resistor_body)
 
     component_box = body.fuse(lid).fuse(hinge)
-
-    resistor_visual = _create_resistor_visual(
-        center=(
-            wall_thickness + inner_length / 2,
-            wall_thickness + inner_width / 2,
-            floor_thickness + inner_height / 2,
-        )
-    )
 
     assembly = LeaderFollowersCuttersPart(component_box)
     assembly.add_named_non_production_part(resistor_visual, "resistor_visual")
