f93994e8973a4ac87c27ac341449f37dd1f27b45
Markus Emmenegger <markus@emmenegger.net>
2026-06-02 10:53:55 +0200

    Optimize tool head geometry for prod

--- src/mege_ender_3v3ke_idex/designs/assemblies/nitehawk_holder_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/nitehawk_holder_assembly.py
@@ -252,6 +252,7 @@ def create_nitehawk_holder_assembly(
     nitehawk_holder_extruder_gap,
     nitehawk_holder_width_offset,
     nitehawk_holder_height_offset,
+    tool_head_additional_mount_plate_clearance,
     BIG_THING,
 ):
     """Create the standalone nitehawk holder assembly."""
@@ -489,7 +490,7 @@ def create_nitehawk_holder_assembly(
             )
             mount_box = align(mount_box, holder_mount_plate, Alignment.CENTER)
             mount_box = align(mount_box, holder_mount_plate, Alignment.FRONT)
-            mount_box = align(mount_box, holder_mount_plate, Alignment.STACK_LEFT)
+            mount_box = align(mount_box, holder_mount_plate, Alignment.STACK_LEFT )
             holder_mount_plate = holder_mount_plate.fuse(mount_box)
 
         holder_mount_plate = align(holder_mount_plate, holder, Alignment.CENTER)
@@ -497,7 +498,7 @@ def create_nitehawk_holder_assembly(
         holder_mount_plate = align(
             holder_mount_plate,
             sprite_extruder,
-            lr.stack_alignment,
+            lr.stack_alignment,stack_gap=tool_head_additional_mount_plate_clearance
         )
         holder_mount_plate = align(holder_mount_plate, holder, Alignment.BACK)
         holder_mount_plate = translate(0, 0, -holder_mount_plate_top_offset)(
