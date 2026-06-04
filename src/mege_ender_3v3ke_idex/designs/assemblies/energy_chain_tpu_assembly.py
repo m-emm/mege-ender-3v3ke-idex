3618e21a1e64c6fa347d6c7feb47256ffc132046
Markus Emmenegger <markus@emmenegger.net>
2026-06-04 10:58:58 +0200

    Start building energy chain

--- src/mege_ender_3v3ke_idex/designs/assemblies/energy_chain_tpu_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/energy_chain_tpu_assembly.py
@@ -28,6 +28,65 @@ def create_energy_chain_tpu_assembly(
 ):
     """Create the initial TPU energy-chain plate skeleton."""
 
+    link_pitch = energy_chain_link_length + energy_chain_link_connector_width
+
+    chain = PartCollector()
+    walls_2 = PartCollector()
+    for i in range(energy_chain_num_links):
+
+        plate = create_box(
+            energy_chain_base_thickness,
+            energy_chain_link_length,
+            energy_chain_width,
+        )
+
+        plate = translate(0, i * link_pitch, 0)(plate)
+        chain = chain.fuse(plate)
+
+        link = create_box(
+            energy_chain_link_connector_thickness,
+            energy_chain_link_connector_width,
+            energy_chain_width,
+        )
+        link = align(link, plate, Alignment.CENTER)
+        link = align(link, plate, Alignment.STACK_BACK)
+        link = align(link, plate, Alignment.LEFT)
+        chain = chain.fuse(link)
+
+        channel_wall_1 = create_box(
+            energy_chain_channel_height,
+            energy_chain_link_length,
+            energy_chain_channel_wall_thickness,
+        )
+
+        channel_wall_1 = align(channel_wall_1, plate, Alignment.CENTER)
+        channel_wall_1 = align(channel_wall_1, plate, Alignment.BOTTOM)
+        channel_wall_1 = align(channel_wall_1, plate, Alignment.STACK_RIGHT)
+        chain = chain.fuse(channel_wall_1)
+
+        channel_link_1 = create_box(
+            energy_chain_channel_link_width,
+            energy_chain_link_length,
+            energy_chain_channel_link_thickness,
+        )
+        channel_link_1 = align(channel_link_1, channel_wall_1, Alignment.CENTER)
+        channel_link_1 = align(channel_link_1, channel_wall_1, Alignment.STACK_RIGHT)
+        channel_link_1 = align(channel_link_1, channel_wall_1, Alignment.BOTTOM)
+        chain = chain.fuse(channel_link_1)
+
+        channel_wall_2 = create_box(
+            energy_chain_width - energy_chain_plug_plate_width,
+            energy_chain_link_length,
+            energy_chain_channel_wall_thickness,
+        )
+
+        channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.CENTER)
+        channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.BOTTOM)
+        channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.STACK_RIGHT)
+        chain = chain.fuse(channel_wall_2)
+
+        walls_2 = walls_2.fuse(channel_wall_1).fuse(channel_wall_2)
+
     unused_parameters = (
         energy_chain_num_links,
         energy_chain_link_connector_thickness,
@@ -49,9 +108,6 @@ def create_energy_chain_tpu_assembly(
     )
     del unused_parameters
 
-    plate = create_box(
-        energy_chain_width,
-        energy_chain_link_length,
-        energy_chain_base_thickness,
-    )
-    return LeaderFollowersCuttersPart(leader=plate)
+    retval = LeaderFollowersCuttersPart(leader=chain)
+    retval.add_named_non_production_part(walls_2, "walls_2")
+    return retval
