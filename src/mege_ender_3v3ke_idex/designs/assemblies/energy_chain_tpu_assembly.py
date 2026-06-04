9cafb1d2248f0dacf9f21c59d209b6f2b4650897
Markus Emmenegger <markus@emmenegger.net>
2026-06-04 11:29:29 +0200

    Add energy chain plug

--- src/mege_ender_3v3ke_idex/designs/assemblies/energy_chain_tpu_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/energy_chain_tpu_assembly.py
@@ -1,6 +1,9 @@
 """TPU energy chain skeleton assembly."""
 
+from mege_ender_3v3ke_idex.designs.plug_and_hole import create_plug
 from shellforgepy.simple import *
+
+BIG_THING = 500
 
 
 def create_energy_chain_tpu_assembly(
@@ -12,6 +15,8 @@ def create_energy_chain_tpu_assembly(
     energy_chain_link_connector_thickness,
     energy_chain_link_connector_width,
     energy_chain_plug_diameter,
+    energy_chain_plug_angle_deg,
+    energy_chain_plug_height,
     energy_chain_plug_slit_width,
     energy_chain_plug_base_thickness,
     energy_chain_plug_fillet_radius,
@@ -29,9 +34,17 @@ def create_energy_chain_tpu_assembly(
     """Create the initial TPU energy-chain plate skeleton."""
 
     link_pitch = energy_chain_link_length + energy_chain_link_connector_width
+    closure_rotation_center = (
+        energy_chain_base_thickness
+        + energy_chain_channel_height
+        + energy_chain_channel_link_width / 2,
+        0,
+        energy_chain_channel_link_thickness * 0.75,
+    )
 
     chain = PartCollector()
     walls_2 = PartCollector()
+    plug_hole_cutters = PartCollector()
     for i in range(energy_chain_num_links):
 
         plate = create_box(
@@ -83,31 +96,52 @@ def create_energy_chain_tpu_assembly(
         channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.CENTER)
         channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.BOTTOM)
         channel_wall_2 = align(channel_wall_2, channel_link_1, Alignment.STACK_RIGHT)
+
+        plug_base = create_box(
+            energy_chain_plug_plate_width - energy_chain_channel_link_width,
+            energy_chain_link_length,
+            energy_chain_channel_wall_thickness + energy_chain_channel_height,
+        )
+
+        plug_base = align(plug_base, channel_wall_2, Alignment.CENTER)
+        plug_base = align(plug_base, channel_wall_2, Alignment.BOTTOM)
+        plug_base = align(plug_base, channel_wall_2, Alignment.STACK_RIGHT)
+
+        channel_wall_2 = channel_wall_2.fuse(plug_base)
+
+        plug = create_plug(
+            plug_diameter=energy_chain_plug_diameter,
+            plug_angle_deg=energy_chain_plug_angle_deg,
+            plug_height=energy_chain_plug_height,
+            plug_wall_thickness=energy_chain_plug_wall_thickness,
+            plug_base_thickness=energy_chain_plug_base_thickness,
+            plug_slit_width=energy_chain_plug_slit_width,
+            fillet_radius=energy_chain_plug_fillet_radius,
+            plug_lip_height=energy_chain_plug_lip_height,
+            plug_lip_size=energy_chain_plug_lip_size,
+            plug_lip_top_gap=energy_chain_plug_lip_top_gap,
+        )
+        plug = align(plug, plug_base, Alignment.CENTER, axes=[0, 1])
+        plug = align(plug, plug_base, Alignment.STACK_TOP)
+        channel_wall_2 = channel_wall_2.fuse(plug)
+
+        hole_cutter = create_cylinder(
+            energy_chain_plug_diameter / 2 + energy_chain_plug_hole_slack,
+            BIG_THING,
+        )
+        hole_cutter = align(hole_cutter, plug, Alignment.CENTER)
+        hole_cutter = rotate(-90, axis=(0, 1, 0), center=closure_rotation_center)(
+            hole_cutter
+        )
+        plug_hole_cutters = plug_hole_cutters.fuse(hole_cutter)
+
         chain = chain.fuse(channel_wall_2)
 
         walls_2 = walls_2.fuse(channel_wall_2)
 
-    unused_parameters = (
-        energy_chain_num_links,
-        energy_chain_link_connector_thickness,
-        energy_chain_link_connector_width,
-        energy_chain_plug_diameter,
-        energy_chain_plug_slit_width,
-        energy_chain_plug_base_thickness,
-        energy_chain_plug_fillet_radius,
-        energy_chain_plug_wall_thickness,
-        energy_chain_plug_lip_height,
-        energy_chain_plug_lip_size,
-        energy_chain_plug_lip_top_gap,
-        energy_chain_plug_hole_slack,
-        energy_chain_channel_wall_thickness,
-        energy_chain_channel_link_thickness,
-        energy_chain_channel_link_width,
-        energy_chain_channel_height,
-        energy_chain_plug_plate_width,
-    )
-    del unused_parameters
+    chain = chain.cut(plug_hole_cutters)
 
     retval = LeaderFollowersCuttersPart(leader=chain)
+    retval.add_named_cutter(plug_hole_cutters, "plug_hole_cutters")
     retval.add_named_non_production_part(walls_2, "walls_2")
     return retval
