bfd334d0b992b75d122a4f5d34f3c86d655b18cb
Markus Emmenegger <markus@emmenegger.net>
2026-07-14 17:15:09 +0200

    Add side walls

--- src/mege_ender_3v3ke_idex/designs/assemblies/idex_tap_t1_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/idex_tap_t1_assembly.py
@@ -22,94 +22,159 @@ def create_idex_tap_t1_assembly(
 ):
     """Create a loose fixed Tap frame around the placed right-side context."""
 
-    fixed_tool_head_mount_size = get_bounding_box_size(fixed_tool_head_mount.leader)
-    mount_flange = create_box(
-        fixed_tool_head_mount_size[0],
-        idex_tap_frame_mount_flange_depth,
-        idex_tap_frame_mount_flange_thickness,
-    )
+    lower_mount_strip_width = 9
+    lower_mount_strip_length = 50
+    lower_mount_strip_thickness = 5
+    carriage_mount_plate_thickness = 4
+    side_wall_thickness = 4
 
-    mount_flange = align(mount_flange, fixed_tool_head_mount, Alignment.CENTER)
-    mount_flange = align(mount_flange, fixed_tool_head_mount, Alignment.STACK_TOP)
-    mount_flange = align(mount_flange, x_axis_carriage, Alignment.STACK_FRONT)
 
-    rail_plate_width = max(idex_tap_shuttle_height, mgn7h_rail_width + 16)
-    rail_plate = create_box(
-        rail_plate_width,
-        idex_tap_frame_thickness,
-        idex_tap_frame_height,
-    )
-    rail_plate = align(rail_plate, mgn7h_rail_with_carriage, Alignment.CENTER)
-    rail_plate = align(rail_plate, fixed_tool_head_mount, Alignment.STACK_BOTTOM)
-    rail_plate = align(rail_plate, mgn7h_rail_with_carriage, Alignment.STACK_BACK)
 
-    rail_plate = mgn7h_rail_with_carriage.use_as_cutter_on(rail_plate)
-    fixed_frame = mount_flange  # .fuse(rail_plate)
+    lower_mount_strips = PartCollector()
+    for lr in [Alignment.LEFT, Alignment.RIGHT]:
 
-    fixed_mount_center = get_bounding_box_center(fixed_tool_head_mount.leader)
-    frame_mount_holes = PartCollector()
-    frame_mount_hole_centers = []
-    frame_mount_hole_radius = (
-        MScrew.from_size(idex_tap_frame_mount_screw_size).clearance_hole_normal / 2
-    )
-    for cutter_name, cutter in fixed_tool_head_mount.get_named_cutter_items():
-        if cutter_name == "extruder_cutout":
-            cutter_center = get_bounding_box_center(cutter)
-            cutter_size = get_bounding_box_size(cutter)
-            cutout = create_box(
-                cutter_size[0],
-                cutter_size[1],
-                idex_tap_frame_mount_flange_thickness + 2,
-            )
-            cutout = align(cutout, mount_flange, Alignment.CENTER)
-            cutout = translate(
-                cutter_center[0] - fixed_mount_center[0],
-                cutter_center[1] - fixed_mount_center[1],
-                0,
-            )(cutout)
-            fixed_frame = fixed_frame.cut(cutout)
-            continue
-
-        if not cutter_name.startswith("hole_drill_"):
-            continue
-        cutter_center = get_bounding_box_center(cutter)
-        cutter_size = get_bounding_box_size(cutter)
-        frame_mount_hole_radius = max(cutter_size[0], cutter_size[1]) / 2
-        frame_mount_hole_centers.append(
-            (
-                cutter_center[0] - fixed_mount_center[0],
-                cutter_center[1] - fixed_mount_center[1],
-            )
+        lower_mount_strip = create_box(
+            lower_mount_strip_width,
+            lower_mount_strip_length,
+            lower_mount_strip_thickness,
         )
 
-    for hole_x, hole_y in sorted(set(frame_mount_hole_centers)):
-        mount_hole = create_cylinder(
-            frame_mount_hole_radius,
-            idex_tap_frame_mount_flange_thickness + 2,
-        )
-        mount_hole = align(mount_hole, mount_flange, Alignment.CENTER)
-        mount_hole = translate(hole_x, hole_y, 0)(mount_hole)
-        fixed_frame = fixed_frame.cut(mount_hole)
-        frame_mount_holes = frame_mount_holes.fuse(mount_hole)
+        lower_mount_strip = align(
+            lower_mount_strip,
+            fixed_tool_head_mount,
+            Alignment.FRONT)
+        
+        lower_mount_strip = align(
+            lower_mount_strip,
+            fixed_tool_head_mount,
+            Alignment.STACK_BOTTOM)
+        
+        lower_mount_strip = align(
+            lower_mount_strip,
+            fixed_tool_head_mount,
+            lr)
+        
+        lower_mount_strips = lower_mount_strips.fuse(lower_mount_strip)
 
-    rail_mount_holes = PartCollector()
-    rail_mount_hole_z = -mgn7h_rail_length / 2 + mgn7h_rail_mount_hole_end_offset
-    rail_mount_last_hole_z = mgn7h_rail_length / 2 - mgn7h_rail_mount_hole_end_offset
-    while rail_mount_hole_z <= rail_mount_last_hole_z + 0.001:
-        rail_hole = create_cylinder(
-            mgn7h_rail_mount_hole_diameter / 2,
-            idex_tap_frame_thickness + 4,
-            direction=(0, 1, 0),
-        )
-        rail_hole = align(rail_hole, rail_plate, Alignment.CENTER)
-        rail_hole = translate(0, 0, rail_mount_hole_z)(rail_hole)
-        fixed_frame = fixed_frame.cut(rail_hole)
-        rail_mount_holes = rail_mount_holes.fuse(rail_hole)
-        rail_mount_hole_z += mgn7h_rail_mount_hole_pitch
+    lower_mount_strips = fixed_tool_head_mount.use_as_cutter_on(lower_mount_strips)
 
-    tap = LeaderFollowersCuttersPart(leader=fixed_frame)
-    tap.add_named_follower(rail_plate, "idex_tap_rail_plate")
-    tap.add_named_cutter(frame_mount_holes, "frame_mount_holes")
-    tap.add_named_cutter(rail_mount_holes, "rail_mount_holes")
+    
+    carriage = mgn7h_rail_with_carriage.get_named_follower("carriage")
+
+    carriage_size = get_bounding_box_size(carriage)
+    fixed_tool_head_mount_size = get_bounding_box_size(fixed_tool_head_mount)
+
+    back_mount_plate = create_box(fixed_tool_head_mount_size[0],
+                                  carriage_mount_plate_thickness, 
+                                  carriage_size[2])
+    
+    back_mount_plate = align(
+        back_mount_plate,
+        carriage,
+        Alignment.CENTER, axes=[2]  )
+    
+    back_mount_plate = align(
+        back_mount_plate,
+        fixed_tool_head_mount,
+        Alignment.CENTER, axes=[0]  )
+    
+    back_mount_plate = align(
+        back_mount_plate,
+        carriage,
+        Alignment.STACK_BACK
+    )
+    
+
+    
+                                  
+                                  
+
+
+
+    back_mount_plate_bbox = get_bounding_box(back_mount_plate)
+    lower_mount_strips_bbox = get_bounding_box(lower_mount_strips)
+
+    back_factor = 0.5
+    new_points = [
+        (back_mount_plate_bbox[0][1], back_mount_plate_bbox[0][2]),
+          (back_mount_plate_bbox[0][1], back_mount_plate_bbox[1][2]),
+          (lower_mount_strips_bbox[1][1], lower_mount_strips_bbox[0][2]),
+            ((lower_mount_strips_bbox[0][1] + lower_mount_strips_bbox[1][1] ) * back_factor, lower_mount_strips_bbox[0][2]),
+        ((lower_mount_strips_bbox[0][1] + lower_mount_strips_bbox[1][1] ) * back_factor, (back_mount_plate_bbox[0][2] +   back_mount_plate_bbox[1][2]) /2 ),
+    ]
+            
+            
+
+
+    part = back_mount_plate.fuse(lower_mount_strips)
+
+
+
+    
+
+    points, faces = create_regular_polygon_geometry(30,5,side_wall_thickness)
+
+
+
+    for i in range(5):
+        points[i][0] = new_points[i][0]
+        points[i][1] = new_points[i][1]
+        points[i+5][0] = new_points[i][0]
+        points[i+5][1] = new_points[i][1]
+
+    
+
+
+
+        
+
+
+
+    
+    face_vertex_maps = convert_to_traditional_face_vertex_maps(points, faces)
+
+    side_wall = create_solid_from_traditional_face_vertex_maps(face_vertex_maps)
+    side_wall = rotate(90)(side_wall)
+
+
+    
+    side_wall = rotate(90, axis=[0,1,0])(side_wall)
+
+    side_wall = align(
+        side_wall,
+        back_mount_plate,
+        Alignment.STACK_FRONT
+    )
+
+    side_wall = align(
+        side_wall,
+        lower_mount_strips,
+        Alignment.RIGHT
+    )
+    side_wall = align(
+        side_wall,
+        lower_mount_strips,
+        Alignment.STACK_BOTTOM
+    )
+
+
+    part = part.fuse(side_wall)
+
+        
+    side_wall_left = align(
+        side_wall,
+        lower_mount_strips,
+        Alignment.LEFT
+    )
+
+    part = part.fuse(side_wall_left)
+
+
+    part = mgn7h_rail_with_carriage.use_as_cutter_on(part)
+
+    tap = LeaderFollowersCuttersPart(leader=part)
+    # tap.add_named_follower(rail_plate, "idex_tap_rail_plate")
+    # tap.add_named_cutter(frame_mount_holes, "frame_mount_holes")
+    # tap.add_named_cutter(rail_mount_holes, "rail_mount_holes")
 
     return tap
