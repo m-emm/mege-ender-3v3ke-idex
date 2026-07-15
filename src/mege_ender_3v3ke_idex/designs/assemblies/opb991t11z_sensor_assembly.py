0f3fc1e4eddbce6748fb49b449e3790bb398f0d0
Markus Emmenegger <markus@emmenegger.net>
2026-07-15 20:08:24 +0200

    cleanup sensor assembly params

--- src/mege_ender_3v3ke_idex/designs/assemblies/opb991t11z_sensor_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/opb991t11z_sensor_assembly.py
@@ -7,19 +7,19 @@ def create_opb991t11z_sensor_assembly(
     *,
     opb991t11z_body_width,
     opb991t11z_body_depth,
-    opb991t11z_body_height,
     opb991t11z_base_height,
     opb991t11z_slot_width,
-    opb991t11z_slot_depth,
-    opb991t11z_aperture_width,
-    opb991t11z_optical_center_z,
     opb991t11z_mount_tab_width,
+    opb991t11z_mount_tab_depth,
+    opb991t11z_mount_tab_thickness,
+    opb991t11z_both_tower_width,
+    opb991t11z_tower_height,
+    opb991t11z_connector_towers_width,
+    opb991t11z_connector_towers_depth,
+    opb991t11z_connector_towers_height,
+    opb991t11z_connector_towers_gap,
     opb991t11z_mount_hole_diameter,
     opb991t11z_mount_hole_pitch,
-    opb991t11z_wire_exit_width,
-    opb991t11z_wire_exit_depth,
-    opb991t11z_wire_diameter,
-    opb991t11z_wire_length,
 ):
     """Create a simplified datasheet-sized OPB991T11Z sensor model."""
 
@@ -29,100 +29,104 @@ def create_opb991t11z_sensor_assembly(
         opb991t11z_base_height,
     )
 
-    tab = create_box(
+    tab = create_rounded_slab(
         opb991t11z_mount_tab_width,
-        opb991t11z_body_depth,
-        opb991t11z_base_height,
+        opb991t11z_mount_tab_depth,
+        opb991t11z_mount_tab_thickness,
+        opb991t11z_mount_tab_depth / 2,
     )
     tab = align(tab, base, Alignment.CENTER)
 
-    tower_width = (opb991t11z_body_width - opb991t11z_slot_width) / 2
-    tower_height = opb991t11z_body_height - opb991t11z_base_height
+    two_towers = create_box(
+        opb991t11z_both_tower_width,
+        opb991t11z_body_depth,
+        opb991t11z_tower_height,
+    )
 
-    left_tower = create_box(tower_width, opb991t11z_body_depth, tower_height)
-    left_tower = align(left_tower, base, Alignment.LEFT)
-    left_tower = align(left_tower, base, Alignment.CENTER, axes=[1])
-    left_tower = align(left_tower, base, Alignment.STACK_TOP)
-
-    right_tower = create_box(tower_width, opb991t11z_body_depth, tower_height)
-    right_tower = align(right_tower, base, Alignment.RIGHT)
-    right_tower = align(right_tower, base, Alignment.CENTER, axes=[1])
-    right_tower = align(right_tower, base, Alignment.STACK_TOP)
-
-    leader = tab.fuse(base).fuse(left_tower).fuse(right_tower)
-
-    mounting_holes = PartCollector()
-    for index, x_offset in enumerate(
-        [-opb991t11z_mount_hole_pitch / 2, opb991t11z_mount_hole_pitch / 2]
-    ):
-        mounting_hole = create_cylinder(
-            opb991t11z_mount_hole_diameter / 2,
-            opb991t11z_base_height + 2,
-        )
-        mounting_hole = align(mounting_hole, tab, Alignment.CENTER, axes=[1])
-        mounting_hole = align(mounting_hole, tab, Alignment.CENTER, axes=[2])
-        mounting_hole = translate(x_offset, 0, 0)(mounting_hole)
-        leader = leader.cut(mounting_hole)
-        mounting_holes = mounting_holes.fuse(mounting_hole)
-        if index == 0:
-            mounting_hole_1 = mounting_hole
-        else:
-            mounting_hole_2 = mounting_hole
-
-    slot_keepout = create_box(
+    slot = create_box(
         opb991t11z_slot_width,
-        opb991t11z_body_depth + 2,
-        opb991t11z_slot_depth,
+        100,
+        100,
     )
-    slot_keepout = align(slot_keepout, leader, Alignment.CENTER, axes=[0, 1])
-    slot_keepout = align(slot_keepout, leader, Alignment.TOP)
+    slot = align(slot, two_towers, Alignment.CENTER)
+    two_towers = two_towers.cut(slot)
 
-    optical_center = create_cylinder(
-        opb991t11z_aperture_width / 2,
-        opb991t11z_body_depth + 2,
-        direction=(0, 1, 0),
+    two_towers = align(two_towers, tab, Alignment.CENTER)
+    two_towers = align(two_towers, tab, Alignment.STACK_TOP)
+
+    connector_towers = create_box(
+        opb991t11z_connector_towers_width,
+        opb991t11z_connector_towers_depth,
+        opb991t11z_connector_towers_height,
     )
-    optical_center = align(optical_center, leader, Alignment.CENTER, axes=[0, 1])
-    optical_center = align(optical_center, leader, Alignment.BOTTOM)
-    optical_center = translate(0, 0, opb991t11z_optical_center_z)(optical_center)
-
-    wire_exit = create_box(
-        opb991t11z_wire_exit_width,
-        opb991t11z_wire_exit_depth,
-        opb991t11z_base_height,
+    connector_towers_slot_cutter = create_box(
+        opb991t11z_connector_towers_gap,
+        100,
+        100,
     )
-    wire_exit = align(wire_exit, leader, Alignment.CENTER, axes=[0])
-    wire_exit = align(wire_exit, leader, Alignment.STACK_FRONT)
-    wire_exit = align(wire_exit, leader, Alignment.BOTTOM)
+    connector_towers_slot_cutter = align(
+        connector_towers_slot_cutter,
+        connector_towers,
+        Alignment.CENTER,
+    )
+    connector_towers_slot_cutter = align(
+        connector_towers_slot_cutter,
+        connector_towers,
+        Alignment.TOP,
+    )
+    connector_towers_slot_cutter = translate(
+        0,
+        0,
+        -opb991t11z_connector_towers_gap / 2,
+    )(connector_towers_slot_cutter)
 
-    wire_bundle = PartCollector()
-    wire_pitch = opb991t11z_wire_diameter * 1.6
-    for wire_index in range(5):
-        wire = create_cylinder(
-            opb991t11z_wire_diameter / 2,
-            opb991t11z_wire_length,
-            direction=(0, 0, 1),
+    connector_towers = connector_towers.cut(connector_towers_slot_cutter)
+    connector_towers_arc_cutter = create_cylinder(
+        opb991t11z_connector_towers_gap / 2,
+        100,
+    )
+    connector_towers_arc_cutter = rotate(90, axis=(1, 0, 0))(
+        connector_towers_arc_cutter
+    )
+    connector_towers_arc_cutter = align(
+        connector_towers_arc_cutter,
+        connector_towers,
+        Alignment.CENTER,
+    )
+    connector_towers_arc_cutter = align(
+        connector_towers_arc_cutter,
+        connector_towers,
+        Alignment.TOP,
+    )
+    connector_towers = connector_towers.cut(connector_towers_arc_cutter)
+
+    connector_towers = align(connector_towers, tab, Alignment.CENTER)
+    connector_towers = align(connector_towers, tab, Alignment.STACK_BOTTOM)
+
+    holes_fused = PartCollector()
+
+    holes = []
+    for i in range(2):
+        hole = create_cylinder(
+            opb991t11z_mount_hole_diameter / 2,
+            100,
         )
-        wire = align(wire, wire_exit, Alignment.CENTER, axes=[1])
-        wire = align(wire, wire_exit, Alignment.STACK_BOTTOM)
-        wire = translate(
-            (wire_index - 2) * wire_pitch,
-            -opb991t11z_wire_exit_depth / 2,
-            0,
-        )(wire)
-        wire_bundle = wire_bundle.fuse(wire)
+        hole = translate(i * opb991t11z_mount_hole_pitch, 0, 0)(hole)
+        holes_fused = holes_fused.fuse(hole)
+        holes.append(hole)
+
+    hole_drills = LeaderFollowersCuttersPart(leader=holes_fused)
+    for i, hole in enumerate(holes):
+        hole_drills.add_named_cutter(hole, f"hole_{i+1}")
+
+    hole_drills = align(hole_drills, tab, Alignment.CENTER)
+    tab = tab.cut(hole_drills.leader)
+
+    leader = tab.fuse(two_towers)
+    leader = leader.fuse(connector_towers)
 
     sensor = LeaderFollowersCuttersPart(leader=leader)
-    sensor.add_named_follower(leader, "body")
-    sensor.add_named_follower(base, "base")
-    sensor.add_named_follower(left_tower, "emitter_tower")
-    sensor.add_named_follower(right_tower, "sensor_tower")
-    sensor.add_named_non_production_part(slot_keepout, "slot_keepout")
-    sensor.add_named_non_production_part(optical_center, "optical_center")
-    sensor.add_named_non_production_part(wire_exit, "wire_exit")
-    sensor.add_named_non_production_part(wire_bundle, "wire_bundle")
-    sensor.add_named_cutter(mounting_holes, "mounting_holes")
-    sensor.add_named_cutter(mounting_hole_1, "mounting_hole_1")
-    sensor.add_named_cutter(mounting_hole_2, "mounting_hole_2")
+    for name, hole in hole_drills.get_named_cutter_items():
+        sensor.add_named_cutter(hole, name)
 
     return sensor
