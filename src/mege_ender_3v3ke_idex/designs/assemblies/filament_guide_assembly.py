6a04ba68b550b0f7c5ffc3b96e3a6a7da60d49f8
Markus Emmenegger <markus@emmenegger.net>
2026-08-21 17:36:32 +0200

    Add filament guide spools cage

--- src/mege_ender_3v3ke_idex/designs/assemblies/filament_guide_assembly.py
+++ src/mege_ender_3v3ke_idex/designs/assemblies/filament_guide_assembly.py
@@ -1,9 +1,12 @@
 """Filament-guide assembly reference geometry."""
 
 from shellforgepy.simple import *
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
-def create_filament_guide_assembly():
+def create_filament_guide_spool():
 
     wheel_thickness = 3
     wheel_z_oversize = 1
@@ -24,8 +27,9 @@ def create_filament_guide_assembly():
     )
 
     bearing_size = get_bounding_box_size(bearing)
+    shaft_cutter = bearing.get_named_cutter("shaft_cutter")
 
-    shaft_cutter_size = get_bounding_box_size(bearing.get_named_cutter("shaft_cutter"))
+    shaft_cutter_size = get_bounding_box_size(shaft_cutter)
     bearing_clearance_cutter_size = get_bounding_box_size(
         bearing.get_named_cutter("clearance_cutter")
     )
@@ -73,6 +77,7 @@ def create_filament_guide_assembly():
     retval.add_named_non_production_part(
         bearing.get_named_non_production_part("ball_cover"), "ball_cover"
     )
+    retval.add_named_cutter(bearing.get_named_cutter("shaft_cutter"), "shaft_cutter")
 
     half_spool_bbox = get_bounding_box(retval)
     half_spool_bbox_center = get_bounding_box_center(retval)
@@ -101,5 +106,125 @@ def create_filament_guide_assembly():
     support_ring = align(support_ring, retval, Alignment.BOTTOM)
 
     retval = retval.fuse(support_ring)
+
+    return retval
+
+
+def create_filament_guide_assembly():
+    frame_depth = 8.5
+    frame_width = 4
+    spacer_thickness = 1.5
+    spacer_bearing_clearance = 0.2
+    spool_gap = 0.1
+    frame_spool_clearance = 1.5
+
+    left_spool = create_filament_guide_spool()
+    left_spool = left_spool.prefixed_copy("left_filament_guide_spool")
+
+    right_spool = create_filament_guide_spool()
+    right_spool = right_spool.prefixed_copy("right_filament_guide_spool")
+
+    right_spool = align(
+        right_spool, left_spool, Alignment.STACK_RIGHT, stack_gap=spool_gap
+    )
+
+    fused = left_spool.fuse(right_spool)
+
+    frame = materialize_bounding_box(
+        fused,
+        y_size=frame_depth,
+        z_enlargement=2 * (frame_width + frame_spool_clearance),
+        x_enlargement=2 * (frame_width + frame_spool_clearance),
+    )
+
+    frame_cutter = materialize_bounding_box(
+        fused,
+        x_enlargement=2 * frame_spool_clearance,
+        y_enlargement=2 * frame_spool_clearance,
+        z_enlargement=2 * frame_spool_clearance,
+    )
+
+    frame = frame.cut(frame_cutter)
+
+    retval = LeaderFollowersCuttersPart(frame)
+
+    retval.add_named_non_production_part(left_spool.leader, "left_filament_guide_spool")
+    retval.add_named_non_production_part(
+        right_spool.leader, "right_filament_guide_spool"
+    )
+
+    retval = retval.merge_except_leader(left_spool)
+    retval = retval.merge_except_leader(right_spool)
+
+    for name, cutter in left_spool.get_named_cutter_items():
+        _logger.info(f"Found left spool cutter: {name}")
+
+    checked_one = False
+
+    spacers = PartCollector()
+
+    for name, cutter in retval.get_named_cutter_items():
+        _logger.info(f"Checking cutter: {name}")
+        checked_one = True
+        if "shaft" in name:
+            clearance_hole_diameter = MScrew.from_size("M5").clearance_hole_normal
+            screw_cutter = create_cylinder(clearance_hole_diameter / 2, 100)
+            screw_cutter = align(screw_cutter, cutter, Alignment.CENTER)
+
+            retval = retval.cut(screw_cutter)
+
+            for bt in [Alignment.BOTTOM, Alignment.TOP]:
+                spacer = create_cylinder(
+                    clearance_hole_diameter / 2 + spacer_thickness,
+                    frame_spool_clearance - spacer_bearing_clearance,
+                )
+                spacer = align(spacer, cutter, Alignment.CENTER)
+                spacer = align(
+                    spacer,
+                    right_spool,
+                    bt.stack_alignment,
+                    stack_gap=spacer_bearing_clearance,
+                )
+                spacer = spacer.cut(screw_cutter)
+                spacers = spacers.fuse(spacer)
+
+    retval = retval.fuse(spacers)
+
+    mount_eye_depth = 20
+    mount_eye_fillet_radius = 2
+    mount_eye_screw_hole_inset = 6
+    mount_eye_screw_size = "M5"
+    mount_eye_hole_diameter = MScrew.from_size(
+        mount_eye_screw_size
+    ).clearance_hole_normal
+
+    mount_eye = create_filleted_box(
+        frame_width,
+        mount_eye_depth,
+        mount_eye_hole_diameter * 3,
+        fillet_radius=mount_eye_fillet_radius,
+        no_fillets_at=[Alignment.RIGHT, Alignment.LEFT, Alignment.BACK],
+    )
+
+    mount_eye = align(mount_eye, retval, Alignment.CENTER)
+
+    mount_eye = align(mount_eye, retval, Alignment.LEFT)
+    mount_eye = align(mount_eye, retval, Alignment.STACK_FRONT)
+
+    mount_screw_cutter = create_cylinder(
+        mount_eye_hole_diameter / 2, mount_eye_depth + 10
+    )
+    mount_screw_cutter = rotate(90, axis=(0, 1, 0))(mount_screw_cutter)
+    mount_screw_cutter = align(mount_screw_cutter, mount_eye, Alignment.CENTER)
+    mount_screw_cutter = align(mount_screw_cutter, mount_eye, Alignment.EDGE_FRONT)
+
+    mount_screw_cutter = translate(0, mount_eye_screw_hole_inset, 0)(mount_screw_cutter)
+
+    mount_eye = mount_eye.cut(mount_screw_cutter)
+
+    retval = retval.fuse(mount_eye)
+
+    if not checked_one:
+        raise RuntimeError("No cutters were found in the filament guide assembly.")
 
     return retval
